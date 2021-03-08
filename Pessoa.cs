8d4c3264583e96abdbb75eb8a4f9cf787b48ff97
Guilherme Bini <77159678+Bini19@users.noreply.github.com>
2021-03-08 14:40:34 -0300

    Add files via upload

---
+++ Pessoa.cs
@@ -0,0 +1,14 @@
+﻿namespace Dado_Uma_Pessoa
+{
+    internal class Pessoa
+    {
+        private string cpf;
+        private string nome;
+
+        public Pessoa(string cpf, string nome)
+        {
+            this.cpf = cpf;
+            this.nome = nome;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LogicaDeProgramaçãoTeste01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Digite um Número obrigado !!"); //Imprime um valor na tela
+
+            Console.ForegroundColor = ConsoleColor.White; //Altera a cor da fonte
+
+            Console.WriteLine("Teste01");
+
+            Console.ReadKey(); //Aguarda o pressionamento de alguma tecla para continuar
+
+            int NumeroDigitado;
+
+            Console.Write("Insira um número:"); //Exibe a mensagem
+
+            NumeroDigitado = Convert.ToInt32(Console.ReadLine()); //Lê e converte a para int o numero informado
+
+            if (NumeroDigitado % 2 == 0)
+            {
+                //Numero Par
+                Console.WriteLine("Par");
+            }
+            else
+            {
+                //Numero Impar
+                Console.WriteLine("Impar");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
